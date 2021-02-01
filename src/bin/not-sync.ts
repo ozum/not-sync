13795191b88042267b79912f3fdd0df88cbab5a0
Özüm Eldoğan <ozum@ozum.net>
2021-02-01 18:04:42 +0300

    fix: don't do anything if no paths provided

--- src/bin/not-sync.ts
+++ src/bin/not-sync.ts
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
 
+/* eslint-disable no-console */
+
 /**
  * Very basic CLI without any dependency which uses default options and outputs minimal log.
  * For more advanced options, please use https://www.npmjs.com/package/not-sync-cli
@@ -16,6 +18,6 @@ const args = process.argv.slice(2);
 const paths = args[0].split(",");
 
 const symlink = (service: ServiceKey, target: string, path: string): void =>
-  console.log(`[not-sync] [${service}] "${path}" is moved to "${target}" and added a symbolic link.`); // eslint-disable-line no-console
+  console.log(`[not-sync] [${service}] "${path}" is moved to "${target}" and added a symbolic link.`);
 
-notSync(paths, { createDirs: true, on: { symlink } });
+if (paths) notSync(paths, { createDirs: true, on: { symlink } });
